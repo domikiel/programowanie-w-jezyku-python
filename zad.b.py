f0d6f8063bbf978a0a0b50e421929d71fb75a135
Dominika Kielesz <dominika.kielesz@edu.uekat.pl>
2025-12-13 23:46:58 +0100

    pycodestyl

--- zad.b.py
+++ zad.b.py
@@ -4,15 +4,16 @@ def version1(numbers):
         result.append(number * 2)
     return result
 
+
 num = [2, 4, 6, 8, 12]
 
 result_for = version1(num)
 print(result_for)
 
 
-
 def version2(numbers):
     return [number * 2 for number in numbers]
+
 
 num = [2, 4, 6, 8, 12]
 result_for = version2(num)
